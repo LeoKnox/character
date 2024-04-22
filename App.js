fd9d3fd2105ab0178576241f19a387f1b88f6f8a
LeoKnox <LeoKnox@users.noreply.github.com>
2024-04-22 11:47:05 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import { useState } from "react";
 import "./styles.css";
 import "./components/Home.js";
+import "./components/Character.js";
 
 export default function App() {
   const [page, setPage] = useState(<Home />);
-  return <div className="App">{page}</div>;
+  return (
+    <div className="App">
+      <button onClick={() => setPage(<Character />)}>Character</button>
+      {page}
+    </div>
+  );
 }
