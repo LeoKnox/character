46c5434917ca211750095d637d7b26382ce3411b
LeoKnox <LeoKnox@users.noreply.github.com>
2024-04-24 13:20:22 -0700

    Update Character.js

--- components/Character.js
+++ components/Character.js
@@ -1,10 +1,14 @@
+import Inventory from "./Inventory.js";
+
 export default Character = () => {
   return (
     <>
       <h1>Character Page</h1>
       <div className="character">
         <div className="stats">S</div>
-        <div className="inventory">I</div>
+        <div className="inventory">
+          <Inventory />
+        </div>
         <div className="abilities">G</div>
       </div>
     </>
