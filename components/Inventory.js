752714fbb409df28f0c7ef1f902e633e35842ea4
LeoKnox <LeoKnox@users.noreply.github.com>
2024-04-25 13:30:47 -0700

    Update Inventory.js

--- components/Inventory.js
+++ components/Inventory.js
@@ -1,12 +1,14 @@
-export default Character = ({getInventory}) => {
+export default Character = ({ getInventory }) => {
   return (
     <>
-    {Object.keys(getInventory)}
-    <dl>
-      <dt>Weapons</dt>
-      <dd>Sword</dd>
-      <dd>Axe</dd>
-    </dl>
+      {Object.keys(getInventory).map((key) => (
+        <button>{key}</button>
+      ))}
+      <dl>
+        <dt>Weapons</dt>
+        <dd>Sword</dd>
+        <dd>Axe</dd>
+      </dl>
     </>
   );
 };
