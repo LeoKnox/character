dfc4b3b8ed0bd7b74cf3e3c20dc39658bc0f5ada
LeoKnox <LeoKnox@users.noreply.github.com>
2024-04-25 13:07:15 -0700

    Create characterData.js

---
+++ components/characterData.js
@@ -0,0 +1,14 @@
+let cd = {
+  name: "Aelien",
+  class: "Fighter",
+  atk: 10,
+  def: 10,
+};
+
+export const getName = () => {
+  return cd.name;
+};
+
+export const getClass = () => {
+  return cd.class;
+};
