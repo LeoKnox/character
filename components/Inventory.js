8607b7239d58df139383bbc971accc89c9146553
LeoKnox <LeoKnox@users.noreply.github.com>
2024-04-26 16:36:52 -0700

    Update Inventory.js

--- components/Inventory.js
+++ components/Inventory.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 export default Character = ({ getInventory }) => {
-    const [type, setType] = useState("pack");
+  const [type, setType] = useState("weapons");
   return (
     <>
       {Object.keys(getInventory).map((key) => (
-        <button>{key}</button>
+        <button onClick={() => setType(key)}>{key}</button>
       ))}
       {getInventory[type].map((item) => (
         <p>{item}</p>
