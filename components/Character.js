969930bcbf4dc991d93668cf1aa8b7183f752d9b
LeoKnox <LeoKnox@users.noreply.github.com>
2024-04-27 15:12:52 -0700

    Update Character.js

--- components/Character.js
+++ components/Character.js
@@ -2,14 +2,14 @@ import Inventory from "./Inventory.js";
 import { getName, getClass, getInventory } from "./characterData.js";
 
 export default Character = () => {
-  console.log(getName());
+  //console.log(getName());
   let myCharacter = {};
   return (
     <>
       <h1>Character Page</h1>
       <div className="character">
         <div className="stats">
-          {getName()}
+          {getName}
           <p>{getClass()}</p>
         </div>
         <div className="inventory">
