2b34c6dac39c38b1c306e2dedf3b5bb8ef633817
LeoKnox <LeoKnox@users.noreply.github.com>
2024-05-03 13:07:47 -0700

    Update App.js

--- App.js
+++ App.js
@@ -2,12 +2,20 @@ import { useState } from "react";
 import "./styles.css";
 import "./components/Home.js";
 import "./components/Character.js";
+import "./components/Adventure.js";
 
 export default function App() {
-  const [page, setPage] = useState(<Home />);
+  const [page, setPage] = useState(<Character />);
   return (
     <div className="App">
-      <button style={{ color:"Blue" }} onClick={() => setPage(<Home />)}>Home</button>
+      <button
+        style={{ color: "Blue" }}
+        disabled={page === <Home />}
+        onClick={() => setPage(<Home />)}
+      >
+        Home
+      </button>
+      <button onClick={() => setPage(<Adventure />)}>Adventure</button>
       <button onClick={() => setPage(<Character />)}>Character</button>
       {page}
     </div>
