f3103fa9d151adcccb3fba7e78e8bd9602c3dd68
LeoKnox <LeoKnox@users.noreply.github.com>
2024-05-07 14:22:04 -0700

    Update roomData.js

--- components/roomData.js
+++ components/roomData.js
@@ -1,6 +1,12 @@
 import { useState } from "react";
 
+export const dungeonData = {
+  roomName: "entry",
+  description: "entrance",
+  length: 5,
+  width: 5,
+};
+
 const roomData = () => {
   return "test";
 };
-
