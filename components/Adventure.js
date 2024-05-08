2b2d6cbaba83af3e60a5d3df4c4ea1c35d9b283f
LeoKnox <LeoKnox@users.noreply.github.com>
2024-05-08 14:23:57 -0700

    Update Adventure.js

--- components/Adventure.js
+++ components/Adventure.js
@@ -6,9 +6,11 @@ export default Adventure = () => {
       <h3>Adventure Home</h3>
       <div>
         {[...Array(dungeonData["width"])].map((e, i) => {
-          return <p key={i}>
-            {i}[...Array(4)].map((e, j) => {"*"}
-          </p>;
+          return (
+            <p key={i}>
+              {[...Array(4)].map((e, j) => ("*"))}
+            </p>
+          );
         })}
       </div>
     </>
