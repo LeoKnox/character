81463916188093a82374c92455578d943490d808
LeoKnox <LeoKnox@users.noreply.github.com>
2024-05-09 13:33:44 -0700

    Update Adventure.js

--- components/Adventure.js
+++ components/Adventure.js
@@ -1,61 +1,40 @@
 import { dungeonData } from "./roomData.js";
 
 export default Adventure = () => {
+    const drawRow = () => {
+     return "!!!!"
+     }
   return (
     <>
       <h3>Adventure Home</h3>
       <div>
         <table>
+        {() => drawRow()}
+          <tr>
+            #
+            {[...Array(dungeonData["length"])].map(() => (
+              <td>#</td>
+            ))}
+            #
+          </tr>
           {[...Array(dungeonData["width"])].map(() => {
             return (
               <tr>
-                +
-                {[...Array(dungeonData["length"])].map(() => (
-                  <td>*</td>
-                ))}
-                +
-              </tr>
-            );
-          })}
-        </table>
-      </div>
-    </>
-  );
-};
-import { dungeonData } from "./roomData.js";
-
-export default Adventure = () => {
-  return (
-    <>
-      <h3>Adventure Home</h3>
-      <div>
-        <table>
-        <tr>
-                o
-                {[...Array(dungeonData["length"])].map(() => (
-                  <td>o</td>
-                ))}
-                o
-              </tr>
-          {[...Array(dungeonData["width"])].map(() => {
-            return (
-                
-              <tr>
-                o
+                #
                 {[...Array(dungeonData["length"])].map(() => (
                   <td>+</td>
                 ))}
-                o
+                #
               </tr>
             );
           })}
           <tr>
-                o
-                {[...Array(dungeonData["length"])].map(() => (
-                  <td>o</td>
-                ))}
-                o
-              </tr>
+            #
+            {[...Array(dungeonData["length"])].map(() => (
+              <td>#</td>
+            ))}
+            #
+          </tr>
         </table>
       </div>
     </>
