28fea839ae3aad74b498ad00cd201e79ae06ad41
LeoKnox <LeoKnox@users.noreply.github.com>
2024-05-09 13:27:54 -0700

    Update Adventure.js

--- components/Adventure.js
+++ components/Adventure.js
@@ -22,3 +22,42 @@ export default Adventure = () => {
     </>
   );
 };
+import { dungeonData } from "./roomData.js";
+
+export default Adventure = () => {
+  return (
+    <>
+      <h3>Adventure Home</h3>
+      <div>
+        <table>
+        <tr>
+                o
+                {[...Array(dungeonData["length"])].map(() => (
+                  <td>o</td>
+                ))}
+                o
+              </tr>
+          {[...Array(dungeonData["width"])].map(() => {
+            return (
+                
+              <tr>
+                o
+                {[...Array(dungeonData["length"])].map(() => (
+                  <td>+</td>
+                ))}
+                o
+              </tr>
+            );
+          })}
+          <tr>
+                o
+                {[...Array(dungeonData["length"])].map(() => (
+                  <td>o</td>
+                ))}
+                o
+              </tr>
+        </table>
+      </div>
+    </>
+  );
+};
