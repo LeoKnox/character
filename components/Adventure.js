718c3d2535bcbf62d4fe7ec915e981905dfde31f
LeoKnox <LeoKnox@users.noreply.github.com>
2024-05-16 15:25:25 -0700

    Update Adventure.js

--- components/Adventure.js
+++ components/Adventure.js
@@ -4,49 +4,27 @@ import { dungeonData } from "./roomData.js";
 export default Adventure = () => {
   useEffect(() => {
     console.log("kiiro");
-    document.getElementById("effectDiv").append("<p>eee</p>");
+    document.getElementById("effectDiv").append(<p>floor</p>);
   }, []);
   const newRoom = [
-    ["+", "+", "+"],
-    ["+", ".", ".", "+"],
+    ["+", "+", "+", "+", "+", "+", "+"],
+    ["+", ".", ".", ".", ".", ".", "+"],
+    ["+", ".", ".", ".", ".", ".", "+"],
+    ["+", ".", ".", ".", ".", ".", "+"],
+    ["+", ".", ".", ".", ".", ".", "+"],
+    ["+", ".", ".", ".", ".", ".", "+"],
+    ["+", "+", "+", "+", "+", "+", "+"],
   ];
-  const roomState = useState(newRoom.map((x) => <tr>{x.map((y) => (<td>{y}</td>))}</tr>));
-  const drawNewRoom = () => {
-    return newRoom.map((data) => (
-      <p>
-        {data.map((tile) => (
-          <label>{tile}</label>
+  const roomState = useState(
+    newRoom.map((x) => (
+      <tr>
+        {x.map((y) => (
+          <td>{y}</td>
         ))}
-      </p>
-    ));
-  };
-  const drawRow = (width, wall, floor) => {
-    return (
-      <>
-        {wall}
+      </tr>
+    ))
+  );
 
-        {[...Array(width)].map(() => (
-          <td>{floor}</td>
-        ))}
-
-        {wall}
-      </>
-    );
-  };
-  const drawRoom = (x, y, wall, floor) => {
-    console.log("draw room");
-    return (
-      <>
-        {[...Array(x)].map((a, b) => (
-          <p>
-            {[...Array(y)].map(() => (
-              <label>{b == 0 ? wall : floor}</label>
-            ))}
-          </p>
-        ))}
-      </>
-    );
-  };
   const test = () => {
     console.log("test");
   };
@@ -54,15 +32,6 @@ export default Adventure = () => {
     <>
       <h3>Adventure Home</h3>
       <div id="testDiv">
-        <table>
-          <tr>{drawRow(dungeonData["width"], "#", "#")}</tr>
-          {[...Array(dungeonData["length"])].map(() => {
-            return <tr>{drawRow(dungeonData["width"], "#", "+")}</tr>;
-          })}
-          <tr>{drawRow(dungeonData["width"], "#", "#")}</tr>
-        </table>
-        {drawNewRoom()}
-        {drawRoom(5, 3, "#", "+")}
         <button onClick={test}>test</button>
         {roomState}
         <div id="effectDiv"></div>
