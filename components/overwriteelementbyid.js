8c84291c00c1a7215d8cd016044f23a166768e3c
LeoKnox <LeoKnox@users.noreply.github.com>
2024-05-17 13:46:20 -0700

    Create overwriteelementbyid.js

---
+++ components/overwriteelementbyid.js
@@ -0,0 +1,7 @@
+document.getElementById(
+      mobArray[0].x + "-" + mobArray[0].y
+    ).innerHTML += `<label id="aka">敵</label>`;
+
+/* needs css "position: absolute" to over write location.
+Can be useful but would need to hide text underneath. Could
+also use "innerHTML =" and replace floor when char moves.
