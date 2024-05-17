5dbe3d1dbaff40f533b58e379dcf655376a46285
LeoKnox <LeoKnox@users.noreply.github.com>
2024-05-17 13:49:20 -0700

    Update overwriteelementbyid.js

--- components/overwriteelementbyid.js
+++ components/overwriteelementbyid.js
@@ -5,3 +5,4 @@ document.getElementById(
 /* needs css "position: absolute" to over write location.
 Can be useful but would need to hide text underneath. Could
 also use "innerHTML =" and replace floor when char moves.
+Setting label "background-color: white" is an option.
