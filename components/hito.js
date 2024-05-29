aee018baf2cc43914a351e5b12023b7ad070dafa
LeoKnox <LeoKnox@users.noreply.github.com>
2024-05-28 19:07:25 -0700

    Update hito.js

--- components/hito.js
+++ components/hito.js
@@ -1,15 +1,7 @@
 import { useState } from "react";
 
 const hitoA = () => {
-  const [hp, setHp] = useState(50);
-
-  const heal = () => {
-    setHp(hp + 1);
-  };
-
-  const damage = () => {
-    setHp(hp - 1);
-  };
+  const [hp, setHp] = useState("50");
 
   return (
     <div>
