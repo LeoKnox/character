9aa505afc888967c0f69631924a6cf856009cf47
LeoKnox <LeoKnox@users.noreply.github.com>
2024-05-30 12:26:37 -0700

    Update Page.js

--- components/Page.js
+++ components/Page.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 export default Page = () => {
+  const [player, setPlayer] = useState({ name: "hito", class: "fighter" });
   return (
     <div>
-      <h3> Page</h3>
+      <h3> {player.name}</h3>
+      <p> {player.class}</p>
     </div>
   );
 };
