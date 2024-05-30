0d16db6c0e94c0af99b7dea9b4225b4219f4275e
LeoKnox <LeoKnox@users.noreply.github.com>
2024-05-30 15:04:22 -0700

    Update Player.js

--- components/Player.js
+++ components/Player.js
@@ -1,12 +1,6 @@
 import { useState } from "react";
 
 export default Player = () => {
-  const [hp, setHp] = useState(10);
-  return (
-    <div>
-      <p>You have {hp} hitpoints!</p>
-
-      <button onClick={() => setHp((hp) => hp + 1)}>Heal</button>
-    </div>
-  );
+  const [player, setPlayer] = useState({ name: "hito", class: "fighter" });
+  return <div>x</div>;
 };
