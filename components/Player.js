4d031398194d016d98393ea468648adc525ccb27
LeoKnox <LeoKnox@users.noreply.github.com>
2024-05-31 17:12:00 -0700

    Update Player.js

--- components/Player.js
+++ components/Player.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import { pagePlayer } from "./pagePlayer.js";
 
 export default Player = () => {
   const [player, setPlayer] = useState({ name: "hito", class: "fighter" });
   return (
     <div>
-      <td>x</td>
+      <td>{pagePlayer.icon}</td>
     </div>
   );
 };
