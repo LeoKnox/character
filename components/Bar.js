7f24b11f83158f19029ea3e4abaaa121b335f1f9
LeoKnox <LeoKnox@users.noreply.github.com>
2024-06-21 15:12:33 -0700

    Update Bar.js

--- components/Bar.js
+++ components/Bar.js
@@ -1,6 +1,9 @@
 export const Bar = ({ label, hp, health }) => {
   return (
-  
-  <p>label: {label}</p>
-  <p>new {hp}/{health}</p>
-)
+    <div>
+      <p>label: {label}</p>
+      <p><progress id="hpBar" value="32" max="100"> 32% </progress></p>
+
+    </div>
+  );
+};
