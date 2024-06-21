9dc78a103d3ad77ebf97bec1c96e778f99531bca
LeoKnox <LeoKnox@users.noreply.github.com>
2024-06-20 17:03:00 -0700

    Update Character.js

--- components/Character.js
+++ components/Character.js
@@ -1,3 +1,10 @@
+import { CharacterSummary } from "./CharacterSummary.js";
+
 export default Character = () => {
-  return <h3>Character</h3>;
+  return (
+    <>
+      <h3>ttt</h3>
+      <CharacterSummary />
+    </>
+  );
 };
