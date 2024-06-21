6c932a6c2f96b921328b35128d84d9e01af5385e
LeoKnox <LeoKnox@users.noreply.github.com>
2024-06-21 15:47:37 -0700

    Update Character.js

--- components/Character.js
+++ components/Character.js
@@ -1,17 +1,23 @@
 import { useState } from "react";
 import { CharacterSummary } from "./CharacterSummary.js";
+import { CharacterMenu } from "./CharacterMenu.js";
 import { characterStats } from "./characterData.js";
 
 export default Character = () => {
   const [characterHealth, setChacterHealth] = useState(characterStats.hp);
   return (
     <>
-      <h3>ttt</h3>
+      <h3>character Summary</h3>
       <CharacterSummary
         health={characterHealth}
         name={characterStats.name}
         level={characterStats.level}
         hp={characterStats.hp}
+      />
+      <CharacterMenu
+        onAttack={() => console.log("attack")}
+        onMagic={() => console.log("magic")}
+        onHeal={() => console.log("heal")}
       />
     </>
   );
