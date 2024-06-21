574ceace8138abf2e5aa2ae61194e2c8b6757679
LeoKnox <LeoKnox@users.noreply.github.com>
2024-06-21 15:17:07 -0700

    Update Character.js

--- components/Character.js
+++ components/Character.js
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import { CharacterSummary } from "./CharacterSummary.js";
+import { characterStats } from "./characterData.js";
 
 export default Character = () => {
+  const [characterHealth, setChacterHealth] = useState(characterStats.hp);
   return (
     <>
       <h3>ttt</h3>
-      <CharacterSummary />
+      <CharacterSummary hp={characterHealth} />
     </>
   );
 };
