918e283e1921adb355eb7053420cbbc4ebf8597d
LeoKnox <LeoKnox@users.noreply.github.com>
2024-06-22 13:13:40 -0700

    Update CharacterMenu.js

--- components/CharacterMenu.js
+++ components/CharacterMenu.js
@@ -1,9 +1,9 @@
-export default CharacterMenu = ({onAttack, onMagic, onHeal}) => {
-    return (
-        <>
-    <div onClick={onAttack}>Attack</div>
-    <div onClick={onMagic}>Magic</div>
-    <div onClick={onHeal}>Heal</div>
+export const CharacterMenu = ({ onAttack, onMagic, onHeal }) => {
+  return (
+    <>
+      <div onClick={onAttack}>Attack</div>
+      <div onClick={onMagic}>Magic</div>
+      <div onClick={onHeal}>Heal</div>
     </>
-    )
-}
+  );
+};
