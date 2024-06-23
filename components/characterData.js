227ace5b9f3d76985293d0259e3e7ef3fd33768f
LeoKnox <LeoKnox@users.noreply.github.com>
2024-06-23 13:40:37 -0700

    Update characterData.js

--- components/characterData.js
+++ components/characterData.js
@@ -40,11 +40,11 @@ export const characterStats = {
   defense: 20,
   magicdefense: 20,
 };
-export const characterAtttack = ({ attacker, receiver }) => {
+export const characterAtttack = (receiver) => {
   console.log(`receiver ${receiver}`);
   const damageReceived =
     // attacker.attack = (attacker.level - receiver.level)* 1.25
     characterStats.magic;
-  const newHealth = receiver.hp - damageReceived;
+  const newHealth = receiver - damageReceived;
   return newHealth;
 };
