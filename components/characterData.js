1bffb8add9b66e2074a843285a31543af8deb59b
LeoKnox <LeoKnox@users.noreply.github.com>
2024-06-23 17:15:14 -0700

    Update characterData.js

--- components/characterData.js
+++ components/characterData.js
@@ -1,28 +1,50 @@
+import { getCharacterInventory } from "./characterInventory.js";
 import { useState } from "react";
-import { CharacterSummary } from "./CharacterSummary.js";
-import { CharacterMenu } from "./CharacterMenu.js";
-import { characterStats, characterAtttack } from "./characterData.js";
 
-export default Character = () => {
-  const [characterHealth, setCharacterHealth] = useState(characterStats.hp);
-  const [damageDone, setDamageDone] = useState(0);
-  return (
-    <>
-      <h3>character Summary</h3>
-      <p>Damage Done: {damageDone}</p>
-      <CharacterSummary
-        health={characterHealth}
-        name={characterStats.name}
-        level={characterStats.level}
-        hp={characterStats.hp}
-      />
-      <CharacterMenu
-        onAttack={() =>
-          setDamageDone(characterAtttack(damageDone, characterStats))
-        }
-        onMagic={() => console.log("magic")}
-        onHeal={() => console.log("heal")}
-      />
-    </>
-  );
+let hpt = 15;
+
+let cd = {
+  name: "Aelien",
+  class: "Fighter",
+  atk: 10,
+  def: 10,
+  inventory: getCharacterInventory(),
+  hp: <p>HP: {hpt}</p>,
+};
+
+export const getName = <h1>{cd.name}</h1>;
+
+export const getClass = cd.class;
+
+export const getInventory = cd.inventory;
+
+export const getAtk = cd.atk;
+
+export const editAtk = (newStat) => {
+  console.log(`new state: ${newStat}`);
+  hpt = newStat;
+};
+
+export const attackAlpha = (factor) => {
+  cd.atk * factor;
+};
+
+export const getHp = cd.hp;
+
+export const characterStats = {
+  name: "Ao",
+  level: 6,
+  hp: 89,
+  attack: 30,
+  magic: 10,
+  defense: 20,
+  magicdefense: 20,
+};
+export const characterAtttack = (damageDone, attacker) => {
+  console.log(`receiver ${damageDone}`);
+  const damageReceived =
+    // attacker.attack = (attacker.level - receiver.level)* 1.25
+    characterStats.magic;
+  const newHealth = damageDone + damageReceived;
+  return newHealth;
 };
