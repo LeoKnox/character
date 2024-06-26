493b61a48937f56483d2a35aae41f72e947d2a79
LeoKnox <LeoKnox@users.noreply.github.com>
2024-06-26 15:03:19 -0700

    Update Character.js

--- components/Character.js
+++ components/Character.js
@@ -13,11 +13,14 @@ export default Character = () => {
   const [characterHealth, setCharacterHealth] = useState(
     characterStats.hp - 30
   );
-  const [damageDone, setDamageDone] = useState([mobStats.hp]);
+  const [damageDone, setDamageDone] = useState([]);
+  for (let i = 0; i < 2; i++) {
+    setDamageDone([...damageDone, mobStats.hp + i]);
+  }
   return (
     <>
       <h3>character Summary</h3>
-      <p>Monster Health: {damageDone[0]}</p>
+      <p>Monster Health: {damageDone[1]}</p>
       <CharacterSummary
         health={characterHealth}
         name={characterStats.name}
